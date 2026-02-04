ff67f7816269a55ccfb947ffadac35be01f91934
sharakashQA <akashshar32@gmail.com>
2026-02-05 03:00:48 +0530

    this is my first commit

---
+++ src/main/java/selenium_basic/FirstDemo.java
@@ -0,0 +1,25 @@
+package selenium_basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstDemo {
+	public void test() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Public\\driver\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		//maximize the browser window
+		driver.manage().window().maximize();
+		//navigate to the url
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+		//retriving title and url of the page
+		String title = driver.getTitle();
+		
+		System.out.println("Title of the page is: " + title);
+		driver.close();
+	}
+	
+	
+
+	
+}
