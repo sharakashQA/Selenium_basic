9b2c97f52fd80d57226203f6798be5b7b2524e9b
sharakashQA <akashshar32@gmail.com>
2026-02-05 14:35:51 +0530

    launch Browser

--- src/main/java/selenium_basic/FirstDemo.java
+++ src/main/java/selenium_basic/FirstDemo.java
@@ -2,29 +2,48 @@ package selenium_basic;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FirstDemo {
-	public void test() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Public\\driver\\chromedriver-win64\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		//maximize the browser window
-		driver.manage().window().maximize();
-		//navigate to the url
-		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-		
-		//retriving title and url of the page
-		String title = driver.getTitle();
-		
-		System.out.println("Title of the page is: " + title);
-		
-		String window=driver.getWindowHandle();
-		
-		driver.close();
-		//purpose of quit is to close all the browser windows opened by selenium
-		driver.quit();
-	}
-	
-	
+	WebDriver driver;
 
-	
+	@BeforeMethod
+	public void setup() {
+		// System.setProperty("webdriver.chrome.driver",
+		// "C:\\Users\\Public\\driver\\chromedriver-win64\\chromedriver.exe");
+
+		// Setup ChromeDriver using WebDriverManager
+		WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        
+		// maximize the browser window
+		driver.manage().window().maximize();
+
+	}
+
+	@Test
+	public void launchurl() {
+		// navigate to the url
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+		// retriving title and url of the page
+		String title = driver.getTitle();
+
+		System.out.println("Title of the page is: " + title);
+
+		String window = driver.getWindowHandle();
+
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
 }
